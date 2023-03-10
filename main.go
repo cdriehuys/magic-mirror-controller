289bc9bf11a03fd28b1fe180feb907aae7105a54
Chathan Driehuys <chathan@driehuys.com>
2023-03-10 04:13:04 +0000

    feat: Web server for display state

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"os/exec"
 	"time"
 )
@@ -30,13 +32,54 @@ func turnOnDisplay(ctx context.Context) error {
 	return nil
 }
 
-func main() {
-	if err := turnOffDisplay(context.Background()); err != nil {
-		log.Fatalln("Error turning off display:", err)
-	}
-	time.Sleep(10 * time.Second)
+type displayState struct {
+	On bool `json:"on"`
+}
 
-	if err := turnOnDisplay(context.Background()); err != nil {
-		log.Fatalln("Error turning on display:", err)
+func displayStateHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPut {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	var newState displayState
+	if err := json.NewDecoder(r.Body).Decode(&newState); err != nil {
+		log.Println("Could not decode new display state:", err)
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		return
+	}
+
+	var updateFunc func(context.Context) error
+	if newState.On {
+		updateFunc = turnOnDisplay
+	} else {
+		updateFunc = turnOffDisplay
+	}
+
+	if err := updateFunc(r.Context()); err != nil {
+		log.Println("Failed to update display state:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if err := json.NewEncoder(w).Encode(newState); err != nil {
+		log.Println("Failed to write display state response:", err)
+	}
+}
+
+func main() {
+	handler := http.NewServeMux()
+	handler.HandleFunc("/display-state", displayStateHandler)
+
+	s := &http.Server{
+		Addr:         ":8080",
+		Handler:      handler,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	log.Println("Starting HTTP server")
+	if err := s.ListenAndServe(); err != nil {
+		log.Fatalln("HTTP server failed:", err)
 	}
 }
